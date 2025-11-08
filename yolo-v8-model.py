2c41003df0701cff1ba7d91eee1448fc4621012a
Ved Pawar <vedpawar1410@gmail.com>
2025-11-08 22:12:50 +0530

    Created yolo-v8-model.py

---
+++ yolo-v8-model.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+model = YOLO('yolov8x')
+results6 = model.predict('input_videos/neutral1.mp4',save=True)
+print(results6[0])
+print('=====================================')
+for box in results6[0].boxes:
+print(box)
